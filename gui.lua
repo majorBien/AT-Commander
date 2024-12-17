cb516e299cf1d5380545863459e3b98dbd59c862
majorBien <shadow7.dev@gmail.com>
2024-12-17 22:28:00 +0100

    tidy up files

---
+++ gui.lua
@@ -0,0 +1,62 @@
+
+
+local loveframes = require("LoveFrames.loveframes")
+
+function createGUI()
+  love.window.setTitle("AT-Commander")
+  love.window.setMode(510, 750)
+
+  local header = loveframes.Create("text")
+  header:SetPos(130,10)
+  header:SetText("AT-Commander")
+  header:SetFont(love.graphics.newFont(33))
+
+  --images load start
+  serialImage = love.graphics.newImage("images/serial.png")
+  bluetoothImage = love.graphics.newImage("images/bluetooth.png")
+  ethernetImage = love.graphics.newImage("images/ethernet.png")
+  wifiImage = love.graphics.newImage("images/wifi.png")
+  --images load end
+
+  local button1 = loveframes.Create("button")
+  button1:SetText("") -- button text
+  button1:SetSize(110, 102)
+  button1:SetPos(20, 60)
+  button1:SetImage(serialImage) -- image setting
+  button1.OnClick = function()
+    print("Serial")
+  end
+
+  local button2 = loveframes.Create("button")
+  button2:SetText("") -- button text
+  button2:SetSize(110, 102)
+  button2:SetPos(140, 60)
+  button2:SetImage(bluetoothImage) -- image setting
+  button2.OnClick = function()
+    print("Bluetooth")
+  end
+
+
+  local button3 = loveframes.Create("button")
+  button3:SetText("") -- button text
+  button3:SetSize(110, 102)
+  button3:SetPos(260, 60)
+  button3:SetImage(wifiImage) -- image setting
+  button3.OnClick = function()
+    print("WiFi")
+  end
+
+  local button4 = loveframes.Create("button")
+  button4:SetText("") -- button text
+  button4:SetSize(110, 102)
+  button4:SetPos(380, 60)
+  button4:SetImage(ethernetImage) -- image setting
+  button4.OnClick = function()
+    print("Ethernet")
+  end
+
+end
+
+return {
+  create = createGUI
+}
