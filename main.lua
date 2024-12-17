634bc513a581187d6a437b31ac73809087b47a57
majorBien <shadow7.dev@gmail.com>
2024-12-17 21:29:47 +0100

    buttton with graphics works

--- main.lua
+++ main.lua
@@ -10,16 +10,46 @@ function love.load()
   header:SetPos(20,10)
   header:SetText("AT-Commander")
   header:SetFont(love.graphics.newFont(20))
+  serialImage = love.graphics.newImage("images/serial.png")
+
+  local button1 = loveframes.Create("button")
+  button1:SetText("") -- button text
+  button1:SetSize(110, 102)
+  button1:SetPos(20, 60)
+  button1:SetImage(serialImage) -- image setting
+  button1.OnClick = function()
+    print("Serial")
+  end
 
 end
 
 function love.update(dt)
-  loveframes.update(dt)
-end
-
+    loveframes.update(dt)
+ end
 
 function love.draw()
     love.graphics.clear(0.15, 0.17, 0.2)
     loveframes.draw()
 end
 
+-- mouse interactions
+function love.mousepressed(x, y, button)
+    loveframes.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+    loveframes.mousereleased(x, y, button)
+end
+
+-- keypad interactions
+function love.keypressed(key, isrepeat)
+    loveframes.keypressed(key, isrepeat)
+end
+
+function love.keyreleased(key)
+    loveframes.keyreleased(key)
+end
+
+function love.textinput(text)
+    loveframes.textinput(text)
+end
